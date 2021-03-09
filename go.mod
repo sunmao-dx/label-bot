9d94159b2f0847eac7a70bffa36978ba6bb3b87a
Clement Li <lizi4@huawei.com>
2021-03-10 00:06:45 +0800

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/SmartsYoung/test
+
+go 1.16
+
+require github.com/widuu/gojson v0.0.0-20170212122013-7da9d2cd949b
+
+require (
+	gitee.com/openeuler/go-gitee v0.0.0-20210201095646-fd9179920723
+	github.com/antihax/optional v1.0.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+)
---
+++ src/api/interface.go
@@ -0,0 +1,47 @@
+package api
+
+
+import (
+	sdk "gitee.com/openeuler/go-gitee/gitee"
+)
+
+// Client interface for Gitee API
+type Client interface {
+	CreatePullRequest(org, repo, title, body, head, base string, canModify bool) (sdk.PullRequest, error)
+	UpdatePullRequest(org, repo string, number int32, title, body, state, labels string) (sdk.PullRequest, error)
+	GetRef(org, repo, ref string) (string, error)
+	GetPRLabels(org, repo string, number int) ([]sdk.Label, error)
+	ListPRComments(org, repo string, number int) ([]sdk.PullRequestComments, error)
+	ListPrIssues(org, repo string, number int32) ([] sdk.Issue, error)
+	DeletePRComment(org, repo string, ID int) error
+	CreatePRComment(org, repo string, number int, comment string) error
+	UpdatePRComment(org, repo string, commentID int, comment string) error
+	AddPRLabel(org, repo string, number int, label string) error
+	RemovePRLabel(org, repo string, number int, label string) error
+
+	AssignPR(owner, repo string, number int, logins []string) error
+	UnassignPR(owner, repo string, number int, logins []string) error
+	GetPRCommits(org, repo string, number int) ([]sdk.PullRequestCommits, error)
+
+	CreateGiteeIssueComment(org, repo string, number string, comment string) error
+
+	IsCollaborator(owner, repo, login string) (bool, error)
+	IsMember(org, login string) (bool, error)
+	GetGiteePullRequest(org, repo string, number int) (sdk.PullRequest, error)
+	GetGiteeRepo(org, repo string) (sdk.Project, error)
+	MergePR(owner, repo string, number int, opt sdk.PullRequestMergePutParam) error
+
+	GetRepos(org string) ([]sdk.Project, error)
+	RemoveIssueLabel(org, repo, number, label string) error
+	AddIssueLabel(org, repo, number, label string) error
+}
+
+type ListPullRequestOpt struct {
+	State           string
+	Head            string
+	Base            string
+	Sort            string
+	Direction       string
+	MilestoneNumber int
+	Labels          []string
+}
