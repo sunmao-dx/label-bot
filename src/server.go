458304d38d3c6b4fb94c6b728cc6e212a6a9cf82
Clement Li <lizi4@huawei.com>
2021-04-08 16:33:49 +0800

    refactor code

--- src/server.go
+++ src/server.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2016 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -34,6 +18,10 @@ var (
 
 func getToken() []byte {
 	return []byte("adb08695039522366c4a645e1e6a3dd4")
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
 }
 
 // ServeHTTP validates an incoming webhook and puts it into the event channel.
@@ -136,6 +124,6 @@ func getLabelsFromREMatches(matches [][]string) (labels []string) {
 }
 
 func main() {
-	http.HandleFunc("/", ServeHTTP)
+	http.HandleFunc("/", HelloHandler)
 	http.ListenAndServe(":8008", nil)
 }
