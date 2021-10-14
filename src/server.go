256dc559a489ad4a1ac23cba3514faff3dd74e52
guoqiangqi <guoqiang.qi1@gmail.com>
2021-10-14 15:18:38 +0800

    Modify regular expression for label matching.

--- src/server.go
+++ src/server.go
@@ -21,11 +21,28 @@ var partiComment []byte
 var partiAiComment []byte
 var token []byte
 var assignComment []byte
-
+var regexpWords = "(mailinglist|maillist|mail|邮件|邮箱|subscribe|订阅" +
+	"|etherpad|meetingrecord" +
+	"|cla|CLA|signagreement|协议|签署" +
+	"|guarding|jenkins|staticcheck|test|compile|robot|测试|编译|检查" +
+	"|website|blog|mirror|下载|官网|博客|镜像" +
+	"|meeting|会议|例会" +
+	"|sensitive|敏感" +
+	"|log|日志" +
+	"|label|标签" +
+	"|access|permission|权限" +
+	"|requirement|featurerequest|需求" +
+	"|translation|翻译" +
+	"|bug|cve|CVE|security" +
+	"|gitee|Gitee|Git|git" +
+	"|sig|Sig|SIG" +
+	"|scheduling|调度" +
+	"|OBS|PRM|ISO" +
+	"|src-openeuler|openeuler|openEuler)"
 var (
-	labelRegex      = regexp.MustCompile(`(?m)^//(comp|sig|good|bug|wg|stat|kind|device|env|ci|mindspore|DFX|usability|user|stage|func|attr|0|1|2)\s*(.*?)\s*$`)
-	labelRegexTitle = regexp.MustCompile(`^(.*)(Lite|LITE)\s*(.*?)\s*$`)
-	labelRegexBody  = regexp.MustCompile(`^(.*)(/ops/|/kernel/|/docs/|/minddata/|/parallel/|/optimizer/|/pynative/|/kernel_compiler/|/device/|/parse/|/cxx_api/|/debug/|/ps/|/pybind_api/|/transform/|/vm/|/communication/|/dataset/|/lite/|/mindrecord/|/nn/|/profiler/|/train/|/model_zoo/|/akg/)\s*(.*?)\s*$`)
+	labelRegex = regexp.MustCompile(`(?m)//(mailing|etherpad|CLA|guarding|website|meeting|kind|bug|CVE|security|activity|gitee|git|sig|release|build|repo|)(\S*)`)
+	// labelRegexTitle = regexp.MustCompile(`^(.*)(Lite|LITE)\s*(.*?)\s*$`)
+	labelRegexWords = regexp.MustCompile(regexpWords)
 )
 
 type Mentor struct {
@@ -84,7 +101,7 @@ func handleIssueEvent(i *gitee.IssueEvent) {
 	org := i.Repository.Namespace
 	repo := i.Repository.Name
 	issueBody := i.Issue.Body
-	issueTitle := i.Issue.Title
+	// issueTitle := i.Issue.Title
 	issueType := i.Issue.TypeName
 	issueInit := i.Issue.Labels
 	assigneeInit := i.Issue.Assignee
@@ -112,28 +129,17 @@ func handleIssueEvent(i *gitee.IssueEvent) {
 			labelsToAdd = getLabelsFromREMatches(labelMatches)
 		}
 
-		issueBody = strings.Replace(issueBody, " ", "", -1)
-		issueBody = strings.Replace(issueBody, "\n", "", -1)
-		var labelFind []string
-		var nameFind []string
-		labelBoMatches := labelRegexBody.FindAllStringSubmatch(issueBody, -1)
+		// var labelFindTi []string
+		// var nameFindTi []string
+		// labelTiMatches := labelRegexTitle.FindAllStringSubmatch(issueTitle, -1)
+		// if len(labelTiMatches) != 0 {
+		// 	nameFindTi = getLabelsFromBodyMatches(labelTiMatches)
+		// }
+		// labelFindTi = getLabel(JsonByte, nameFindTi)
 
-		if len(labelBoMatches) != 0 {
-			nameFind = getLabelsFromBodyMatches(labelBoMatches)
-		}
-		labelFind = getLabel(JsonByte, nameFind)
-
-		var labelFindTi []string
-		var nameFindTi []string
-		labelTiMatches := labelRegexTitle.FindAllStringSubmatch(issueTitle, -1)
-		if len(labelTiMatches) != 0 {
-			nameFindTi = getLabelsFromBodyMatches(labelTiMatches)
-		}
-		labelFindTi = getLabel(JsonByte, nameFindTi)
-
-		if len(labelFindTi) != 0 {
-			labelsToAdd = append(labelsToAdd, labelFindTi...)
-		}
+		// if len(labelFindTi) != 0 {
+		// 	labelsToAdd = append(labelsToAdd, labelFindTi...)
+		// }
 
 		switch issueType {
 		case "Bug-Report":
@@ -178,6 +184,30 @@ func handleIssueEvent(i *gitee.IssueEvent) {
 			return
 		}
 
+		issueBody = strings.Replace(issueBody, " ", "", -1)
+		issueBody = strings.Replace(issueBody, "\n", "", -1)
+		var labelFind []string
+		var nameFind []string
+		labelBoMatches := labelRegexWords.FindAllStringSubmatch(issueBody, -1)
+
+		if len(labelBoMatches) != 0 {
+			for _, tt := range labelBoMatches {
+				for _, t := range tt {
+					fmt.Printf("labelMatches from noteBody:\n%s \n", t)
+				}
+			}
+			nameFind = getLabelsFromBodyMatches(labelBoMatches)
+		}
+
+		if len(nameFind) != 0 {
+			fmt.Println("debug message 1:")
+			for _, name := range nameFind {
+				fmt.Printf("%s ", name)
+			}
+		}
+
+		labelFind = getLabel(JsonByte, nameFind)
+
 		if len(labelFind) != 0 {
 			for _, strLabel := range labelFind {
 				strLabels = strLabels + "**//" + strLabel + "**" + "\n"
@@ -185,7 +215,12 @@ func handleIssueEvent(i *gitee.IssueEvent) {
 			partiTemp := string(partiComment[:])
 			helloWord := ""
 			helloWord = strings.Replace(partiTemp, "{"+"issueMaker"+"}", fmt.Sprintf("%v", issueMaker), -1)
-			helloWord = strings.Replace(helloWord, "{"+"assignee"+"}", fmt.Sprintf("%v", assignee), -1)
+
+			if assignee != "" {
+				helloWord = strings.Replace(helloWord, "{"+"assignee"+"}", fmt.Sprintf("%v", assignee), -1)
+			} else {
+				helloWord = strings.Replace(helloWord, "@"+"{"+"assignee"+"}", fmt.Sprintf("%v", ""), -1)
+			}
 
 			if strings.Contains(strLabels, "good-first-issue") {
 				helloWord = strings.Replace(helloWord, "{"+"goodissue"+"}", fmt.Sprintf("%v", ", 因为这个issue看起来是文档类问题, 适合新手开发者解决"), -1)
@@ -285,7 +320,7 @@ func handleIssueCommentEvent(i *gitee.NoteEvent) {
 	if i.Issue.Assignee != nil {
 		assignee = i.Issue.Assignee.Login
 		assigneeStr = " @" + assignee + " "
-		fmt.Printf("CODE0:Issue %s was assigned to %s", issueNum, assignee)
+		fmt.Printf("CODE0:Issue %s was assigned to %s \n", issueNum, assignee)
 	}
 	labelStrs := make([]string, 0)
 	for _, o := range labels {
@@ -297,11 +332,19 @@ func handleIssueCommentEvent(i *gitee.NoteEvent) {
 		if len(labelMatches) == 0 {
 			return
 		}
+		for _, tt := range labelMatches {
+			for _, t := range tt {
+				fmt.Printf("labelMatches from noteBody:\n%s \n", t)
+			}
+		}
 		var labelsToAdd []string
 		labelsToAdd = getLabelsFromREMatches(labelMatches)
+		for _, labelTemp := range labelsToAdd {
+			fmt.Printf("labelGet from matches: \n%s \n", labelTemp)
+		}
 
 		if strings.Contains(noteBody, "good-first-issue") {
-			astr := "如果您是第一次贡献社区，可以参考我们的贡献指南：https://gitee.com/mindspore/mindspore/blob/master/CONTRIBUTING.md"
+			astr := "如果您是第一次贡献社区，可以参考我们的贡献指南：https://gitee.com/openeuler/blob/master/CONTRIBUTING.md"
 			res := c.CreateGiteeIssueComment(org, repo, issueNum, astr)
 			if res != nil {
 				fmt.Println(res.Error())
@@ -317,7 +360,7 @@ func handleIssueCommentEvent(i *gitee.NoteEvent) {
 		if assignee == "" {
 			assignee = getLabelAssignee(JsonByte, labelsToAdd)
 			assigneeStr = " @" + assignee + " "
-			fmt.Printf("CODE1:Issue %s was assigned to %s", issueNum, assignee)
+			fmt.Printf("CODE1:Issue %s was assigned to %s \n", issueNum, assignee)
 		}
 		rese := c.AssignGiteeIssue(org, repo, labelsToAddStr, issueNum, assignee)
 		if rese != nil {
@@ -381,7 +424,7 @@ func getLabelsFromREMatches(matches [][]string) []string {
 func getLabelsFromBodyMatches(matches [][]string) []string {
 	var labels []string
 	for _, match := range matches {
-		label := strings.ToLower(strings.TrimSpace(strings.Trim(match[2], "/")))
+		label := strings.ToLower(strings.TrimSpace(match[0]))
 		labels = append(labels, label)
 	}
 	return labels
